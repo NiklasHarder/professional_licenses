582b0e83a21e1a3b749c59ee190780d54dff997e
NiklasHarder <Niklas.Harder@uni-konstanz.de>
2016-10-14 18:18:23 -0700

    Added more code

--- professional_licenses.R
+++ professional_licenses.R
@@ -2,6 +2,29 @@
 
 # Script originally written by: Niklas Harder
 
+# Further contributors: 
 
+# IPL members can download the data here: 
 # https://www.dropbox.com/s/roxy7bg9rvwmsri/breeze00.csv?dl=0
+
+# Set the working directoty, please change accordingly
+setwd("C:/Users/nharder/Dropbox (IPL)/prof_licenses")
+
+# Load packages -----------------------------------------------------------
+
+library("foreign")
+library("lubridate")
+
+# Reading the data --------------------------------------------------------
+
+b0 <- read.csv("breeze00.csv")
+
+# Write data as .dta ------------------------------------------------------
+
+write.dta(b0, "breeze00.dta")
+
+# Define dates ------------------------------------------------------------
+
+b0$issue <- mdy(b0$Original.Issue.Date)
+b0$expi <- mdy(b0$Expiration.Date)
 
